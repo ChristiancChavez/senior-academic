ed6d8d0f72f1835daeb82daa51b72e1e18da1f29
Christian Chavez <christiancchavez10@hotmail.com>
2019-01-25 12:55:53 -0500

    Connect with Firebase

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyA2OXQ2V-SNgoKhZWP5uLLFxAPIAMuQe1U",
+    authDomain: "seniors-academic-839c7.firebaseapp.com",
+    databaseURL: "https://seniors-academic-839c7.firebaseio.com",
+    projectId: "seniors-academic-839c7",
+    storageBucket: "seniors-academic-839c7.appspot.com",
+    messagingSenderId: "361716339409"
+};
+firebase.initializeApp(config);
+export default firebase;
